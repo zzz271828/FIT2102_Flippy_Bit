4ba6d3bb04dfbc37d6db2395004bef5d87a830c9
bi8boOoOow <lco271828@gmail.com>
2026-08-24 10:51:39 +1000

    mimic the workshop asteroids and seperate the things in main into many files

---
+++ Assignment1/src/util.ts
@@ -0,0 +1,47 @@
+import { Observable } from "rxjs";
+import { scan, map } from "rxjs/operators";
+
+export { RNG, createRngStreamFromSource, rangeScale };
+
+/** random number **/
+/**
+ * A random number generator which provides two pure functions
+ * `hash` and `scale`. Call `hash` repeatedly to generate the
+ * sequence of hashes.
+ */
+abstract class RNG {
+    private static m = 0x80000000; // 2^31
+    private static a = 1103515245;
+    private static c = 12345;
+
+    public static hash = (seed: number): number =>
+        (RNG.a * seed + RNG.c) % RNG.m;
+
+    public static scale = (hash: number): number =>
+        (2 * hash) / (RNG.m - 1) - 1; // in [-1, 1]
+}
+
+
+/**
+ * Converts values in a stream to random numbers in the range [-1, 1]
+ *
+ * This usually would be implemented as an RxJS operator, but that is currently
+ * beyond the scope of this course.
+ *
+ * @param source$ The source Observable, elements of this are replaced with random numbers
+ * @param seed The seed for the random number generator
+ */
+function createRngStreamFromSource<T>(source$: Observable<T>) {
+    return function createRngStream(seed: number = 0): Observable<number> {
+        const randomNumberStream = source$.pipe(
+            scan(oldSeed => RNG.hash(oldSeed), seed),
+            map(val => RNG.scale(val)),
+        );
+        return randomNumberStream;
+    };
+}
+
+
+function rangeScale(num: number, floor: number, ceiling: number): number {
+    return floor + ((num + 1) / 2) * (ceiling - floor);
+}
